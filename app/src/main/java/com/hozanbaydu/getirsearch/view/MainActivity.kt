7a06cee5c161280306a6d1f229d548c7ad5fb496
Hozan Baydu <hozan.baydu3447@gmail.com>
2022-11-28 08:55:55 +0300

    Mainactivity fragmentlerin bağlanması

--- app/src/main/java/com/hozanbaydu/getirsearch/view/MainActivity.kt
+++ app/src/main/java/com/hozanbaydu/getirsearch/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.hozanbaydu.getirsearch
+package com.hozanbaydu.getirsearch.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hozanbaydu.getirsearch.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
