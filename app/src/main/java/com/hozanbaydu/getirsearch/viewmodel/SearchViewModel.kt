0ec2628862582ac7921e11654497d484c43e5daf
Hozan Baydu <hozan.baydu3447@gmail.com>
2022-12-02 15:15:06 +0300

    viewmodel oluşturulması

--- app/src/main/java/com/hozanbaydu/getirsearch/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/hozanbaydu/getirsearch/viewmodel/SearchViewModel.kt
@@ -1,4 +1,37 @@
 package com.hozanbaydu.getirsearch.viewmodel
 
-class SearchViewModel {
+import android.content.Context
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.hozanbaydu.getirsearch.firebase.GetData
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class SearchViewModel @Inject constructor(
+
+    private val getFromFirebase: GetData
+
+):ViewModel(){
+
+     fun getFood(foodList: MutableLiveData<MutableList<String>>,context: Context){
+        getFromFirebase.getData(foodList,context)
+    }
+
+    fun resultData(restaurantNameText: TextView, foodNameTextView: TextView, priceTextView: TextView, id:String, context: Context,imageView: ImageView){
+
+        getFromFirebase.resultData(restaurantNameText,foodNameTextView,priceTextView,id,context,imageView)
+    }
+
+
+
+    val dataList= MutableLiveData<MutableList<String>>()
+    var foodArray=getFromFirebase.foodArray
+    var idArray=getFromFirebase.idArray
+    var imortanceist=ArrayList<Int>()
+    var minFood=String()
+
 }
