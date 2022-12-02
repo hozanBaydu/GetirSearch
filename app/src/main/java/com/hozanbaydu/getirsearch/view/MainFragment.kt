b96bb00521388f11c93584aa90428bbfa5d1f0e2
Hozan Baydu <hozan.baydu3447@gmail.com>
2022-12-02 15:14:02 +0300

    main fragment binding işlemleri

--- app/src/main/java/com/hozanbaydu/getirsearch/view/MainFragment.kt
+++ app/src/main/java/com/hozanbaydu/getirsearch/view/MainFragment.kt
@@ -1,9 +1,31 @@
 package com.hozanbaydu.getirsearch.view
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.hozanbaydu.getirsearch.R
+import com.hozanbaydu.getirsearch.databinding.FragmentMainBinding
+import com.hozanbaydu.getirsearch.firebase.GetData
 
 class MainFragment:Fragment(R.layout.fragment_main) {
 
+    private var fragmentBinding : FragmentMainBinding? = null
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentMainBinding.bind(view)
+        fragmentBinding = binding
+
+
+
+
+        binding.fikirButton.setOnClickListener {
+
+            findNavController().navigate(MainFragmentDirections.actionMainFragmentToSearchFragment())
+        }
+
+
+    }
 }
