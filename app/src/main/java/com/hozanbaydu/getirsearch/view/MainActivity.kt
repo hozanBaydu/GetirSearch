b87c6c9b4d4b86f8dbd81076c3de97f4e1925b7b
Hozan Baydu <hozan.baydu3447@gmail.com>
2022-12-02 15:13:35 +0300

    navigasyon tasarımı

--- app/src/main/java/com/hozanbaydu/getirsearch/view/MainActivity.kt
+++ app/src/main/java/com/hozanbaydu/getirsearch/view/MainActivity.kt
@@ -3,10 +3,20 @@ package com.hozanbaydu.getirsearch.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hozanbaydu.getirsearch.R
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var fragmentFactory: FragmentFactory
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportFragmentManager.fragmentFactory=fragmentFactory
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
     }
 }
